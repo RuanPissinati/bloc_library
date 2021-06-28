abd24fad46c841a56725c10c846808c8f531ea11
Ruan Pissinati <ruan.revoredo@osbr.com.br>
2021-06-27 22:49:19 -0300

    [feature] Bloc. Criando um Business Logic Component para um contador

--- lib/main.dart
+++ lib/main.dart
@@ -1,171 +1,37 @@
-import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
 
 enum CounterEvent { increment, decrement}
-// Até agora, definimos os dois eventos aos quais nosso aplicativo
-// responderá: CounterEvent.increment e CounterEvent.decrement.
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc():super(0);
 
-// Agora precisamos definir como representar o estado do nosso aplicativo.
-
-// Como estamos construindo um contador, nosso estado é muito simples: é apenas
-// um número inteiro que representa o valor atual do contador. [...] Neste caso, um
-// tipo primitivo é perfeitamente adequado como representação de estado.
-
-
-// Transição
-// A mudança de um estado para outro é chamada de transição. Uma transição
-// consiste no estado atual, no evento e no próximo estado.
-// EXP:
-// {
-// "currentState": 0,
-// "event": "CounterEvent.increment",
-// "nextState": 1
-// }
-
-// STREAM Um fluxo é uma sequência de dados assíncronos.
-// Example(simplify):
-// sumStream(stream) async {
-//   int sum = 0;
-//   await for (value in stream) {
-//     sum += value;
-//   }
-//   return sum;
-// }
-
-Stream<int> countStream(int max) async* {
-  for (int i = 0; i < max; i++) {
-    yield i;
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async*{
+    switch (event) {
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+    }
   }
-}
 
-Future<int> sumStream(Stream<int> stream) async {
-  int sum = 0;
-  await for (int value in stream) {
-    sum += value;
+  @override
+  void onTransition(Transition<CounterEvent, int> transition) {
+    super.onTransition(transition);
+    print(transition);
   }
-  return sum;
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print('$error, $stackTrace');
+  }
 }
 
 void main() async {
-  /// Initialize a stream of integers 0-9
-  Stream<int> stream = countStream(5);
-  /// Compute the sum of the stream of integers
-  int sum = await sumStream(stream);
-  /// Print the sum
-  print(sum); // 45
-}
+  CounterBloc bloc = CounterBloc();
 
-// void main() {
-//   runApp(MyApp());
-// }
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  for (int i = 0; i < 3; i++) {
+    bloc.add(CounterEvent.increment);
   }
 }
